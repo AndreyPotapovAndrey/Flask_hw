7847f23d78ec8a8e67ed21dbb78660394ae9a0d5
Andrey <125595526+AndreyPotapovAndrey@users.noreply.github.com>
2023-12-09 00:52:28 +0300

    final_3 commit

--- app/client.py
+++ app/client.py
@@ -1,9 +1,22 @@
 import requests
 
-# response = requests.post("http://127.0.0.1:5000/ads",
-#                          json={"title": "Iphone X",
-#                                "description": "Prodam nedorogo",
-#                                "owner": "Andrey"})
+response = requests.post(
+    "http://127.0.0.1:5000/ads",
+    json={"title": "Iphone X", "description": "Prodam", "owner": "Andrey"},
+)
+print(response.status_code)
+print(response.text)
+
+id = response.json()["id"]
+response = requests.get(
+    f"http://127.0.0.1:5000/ads/{id}",
+)
+print(response.status_code)
+print(response.text)
+
+# response = requests.patch("http://127.0.0.1:5000/ads/1",
+#                           json={"title": "Iphone 13",
+#                                 "description": "Prodam ochen dorogo"}, )
 # print(response.status_code)
 # print(response.text)
 #
@@ -13,28 +26,16 @@ import requests
 # )
 # print(response.status_code)
 # print(response.text)
-
-# response = requests.patch("http://127.0.0.1:5000/ads/1",
-#                           json={"title": "Iphone 13",
-#                                 "description": "Prodam ochen dorogo"}, )
-# print(response.status_code)
-# print(response.text)
 #
-# response = requests.get(
+# response = requests.delete(
 #     "http://127.0.0.1:5000/ads/1",
 # )
 # print(response.status_code)
 # print(response.text)
 
-response = requests.delete(
-    "http://127.0.0.1:5000/ads/1",
-)
-print(response.status_code)
-print(response.text)
 
-
-response = requests.get(
-    "http://127.0.0.1:5000/ads/1",
-)
-print(response.status_code)
-print(response.text)
+# response = requests.get(
+#     "http://127.0.0.1:5000/ads/1",
+# )
+# print(response.status_code)
+# print(response.text)
